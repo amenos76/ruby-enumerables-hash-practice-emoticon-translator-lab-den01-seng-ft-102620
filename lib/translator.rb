7b71241d82b595ebe0218fe6c2bc519ad1409cb8
Augie Menos <augie.menos@gmail.com>
2020-10-18 04:12:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,11 @@
 # require modules here
+require 'yaml'
+require 'pry'
+#emoticon_file = YAML.load_file('./lib/emoticons.yml')
 
-def load_library
-  # code goes here
-end
 
-def get_japanese_emoticon
-  # code goes here
-end
+def load_library(path)
+emoticons = YAML.load_file('./lib/emoticons.yml')
 
-def get_english_meaning
-  # code goes here
+
 end
